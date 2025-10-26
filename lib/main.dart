250dd3429a1be84217665d209605521db14da945
Amara Irobi <beaamara6@gmail.com>
2025-10-26 16:29:58 -0400

    restored main.dart to have share feature

--- lib/main.dart
+++ lib/main.dart
@@ -1231,15 +1231,8 @@ class _GenerateCardState extends State<generateCard> with SingleTickerProviderSt
           context,
           MaterialPageRoute(
             builder: (_) => RecipeDetailScreen(recipeId: widget.recipeId),
-          
           ),
-       ).then((result) {
-        if (result is int) {
-          // jumps to selected tab index
-          _MyHomePageState? homeState = context.findAncestorStateOfType<_MyHomePageState>();
-          homeState?._tabController.animateTo(result);
-        }
-       });
+        );
       },
       child: FadeTransition(
         opacity: _opacityAnimation,
